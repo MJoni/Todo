42b8c416d6b5e88bf12a300e33068007bac810e4
Md Joni <mjoni@qa.com>
2020-10-05 20:38:22 +0100

    Add initial test class for automation selenium test

---
+++ src/test/java/com/qa/selenium/TodoWebSiteTest.java
@@ -0,0 +1,47 @@
+package com.qa.selenium;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.Assert.assertEquals;
+
+public class TodoWebSiteTest {
+
+    private WebDriver driver;
+
+    @Before
+    public void setup() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/Driver/chromedriver.exe");
+        driver = new ChromeDriver();
+        Thread.sleep(2000);
+        driver.manage().window().setSize(new Dimension(1366, 768));
+
+    }
+
+    @Test
+    public void webTitleTest(){
+        driver.get("http://127.0.0.1:5500/index.html");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        assertEquals("QAC Home", driver.getTitle());
+    }
+    @Test
+    public void test() throws InterruptedException {
+        driver.get("https://bing.com");
+        Thread.sleep(2000);
+
+        assertEquals("Bing", driver.getTitle());
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+}

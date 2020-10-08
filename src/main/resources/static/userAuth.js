0d514ade057a1cc682aedd916a1087f807b1c5fe
Md Joni <mjoni@qa.com>
2020-10-08 21:41:02 +0100

    Single folder for a javascript files

--- src/main/resources/static/userAuth.js
+++
@@ -1,8 +0,0 @@
-let userAuth =document.cookie;
-console.log(userAuth)
-if(userAuth != null || userAuth != undefined){
-    console.log("Session started")
-    console.log(userAuth)
-}else{
-    console.log("Session timed out")
-}
